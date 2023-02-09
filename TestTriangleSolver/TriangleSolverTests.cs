adcdf226c9669044298e0da76f8a7890920aa5ba
jPartridge96 <99056037+jPartridge96@users.noreply.github.com>
2023-02-09 14:47:21 -0500

    Added tests for Equilateral

---
+++ TestTriangleSolver/TriangleSolverTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using TriangleSolver;
+
+namespace TestTriangleSolver
+{
+    [TestFixture]
+    public class TriangleSolverTests
+    {
+        Triangle t = new Triangle();
+
+        #region EQUILATERAL
+        [Test]
+        public void triangle_a5b5c5_expectEquilateral()
+        {
+            // Arrange
+            int a = 5;
+            string expected = "Based on all sides being equal, the type of triangle is an EQUILATERAL";
+
+            // Act
+            string actual = t.AnalyzeTriangle(a, a, a);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        #endregion
+    }
+}
